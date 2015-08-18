f8d0d76ea0aa780ad9189238f73fd743f02c16df
dfenjves <fenjves.daniel@gmail.com>
2015-08-18 18:29:19 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -4,7 +4,7 @@
 //
 //  Created by Chris Gonzales on 5/28/14.
 //  Copyright (c) 2014 FIS. All rights reserved.
-//
+// @"Welcome, Edsger Dijkstra! You'll be in dressing room 2."
 
 #import "FISAppDelegate.h"
 
@@ -16,15 +16,27 @@
 }
 
 - (NSString *)badgeForSpeaker:(NSString *)speaker {
-    return nil;
+    NSMutableString *greet = [@"Hello, my name is " mutableCopy];
+    [greet appendString: speaker];
+    [greet appendString: @"."];
+    return greet;
 }
 
 - (NSArray *)badgesForSpeakers:(NSArray *)speakers {
-    return nil;
+    NSMutableArray *badges =[[NSMutableArray alloc]init];
+    for (NSInteger i = 0; i < [speakers count]; i++) {
+        [badges addObject:[self badgeForSpeaker:speakers[i]]];
+    }
+    return badges;
 }
 
 - (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
-    return nil;
+    NSMutableArray *rooms =[[NSMutableArray alloc]init];
+    for (NSInteger i = 0; i < [speakers count]; i++) {
+        NSString *assignment = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %ld.", speakers[i], i+1];
+        [rooms addObject:assignment];
+        }
+    return rooms;
 }
 
 @end
